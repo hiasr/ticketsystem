0c1e93d8ea81ac74ef2d97ce2eed5b0c43b6f9e0
Ruben Hias <ruben.hias@gmail.com>
2023-04-10 17:15:48 +0200

    Diverse updates

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,33 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import { MantineProvider } from "@mantine/core";
+import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+    return (
+        <MantineProvider
+            withGlobalStyles
+            withNormalizeCSS
+            theme={{
+                colorScheme: "light",
+                fontFamily: "Open Sans, sans serif",
+                primaryShade: { light: 5, dark: 7 },
+                colors: {
+                    "vtk-yellow": [
+                        "#E4C428",
+                        "#E8C721",
+                        "#EDCA19",
+                        "#F3CD11",
+                        "#F9D009",
+                        "#FFD400",
+                        "#F6CE06",
+                        "#EEC80C",
+                        "#E6C212",
+                        "#DEBD17",
+                    ],
+                },
+            }}
+        >
+            <Component {...pageProps} />
+        </MantineProvider>
+    );
 }
